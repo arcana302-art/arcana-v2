5ec399bf35ce8203d9dd102b1dcef81084800583
arcana302-art <arcana302@gmail.com>
2025-09-07 16:41:31 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import SiteHeader from "@/components/SiteHeader";
 import Hero from "@/components/Hero";
 import HowItWorks from "@/components/HowItWorks";
 import Benefits from "@/components/Benefits";
@@ -14,19 +15,24 @@ import SiteFooter from "@/components/SiteFooter";
 export default function Page() {
   return (
     <main className="bg-[#17031F] text-white">
-      <Hero />
+      <SiteHeader />
+      {/* padding-top para que el hero no quede bajo el header fijo */}
+      <div className="pt-16">
+        <Hero />
 
-      <HowItWorks />
-      <Benefits />
-      <Categories />
-      <FeaturedGuides />
-      <MidCTA />
-      <Testimonials />
-      <Security />
-      <FAQ />
-      <Freebies />
-      <JoinAsExpert />
-      <SiteFooter />
+        <HowItWorks />
+        <Benefits />
+        <Categories />
+        <FeaturedGuides />
+        <MidCTA />
+        <Testimonials />
+        <Security />
+        <FAQ />
+        <Freebies />
+        <JoinAsExpert />
+        <SiteFooter />
+      </div>
     </main>
   );
 }
+
