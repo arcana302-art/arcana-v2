723643e5c219fc662219295fa16898c1c49903b8
arcana302-art <arcana302@gmail.com>
2025-10-05 21:26:05 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
-import Hero from "@/components/Hero";
-import Gancho from "@/components/Gancho";
-import HowItWorks from "@/components/HowItWorks";
-import Benefits from "@/components/Benefits";
-import Categories from "@/components/Categories";
-import FeaturedGuides from "@/components/FeaturedGuides";
-import MidCTA from "@/components/MidCTA";
-import Testimonials from "@/components/Testimonials";
-import Security from "@/components/Security";
-import FAQ from "@/components/FAQ";
-import Freebies from "@/components/Freebies";
-import JoinAsExpert from "@/components/JoinAsExpert";
-import CartaNatalAvanzada from "@/components/CartaNatalAvanzada";
+// app/page.tsx
+import Hero from "./components/Hero";
+import Gancho from "./components/Gancho";
+import HowItWorks from "./components/HowItWorks";
+import Benefits from "./components/Benefits";
+import Categories from "./components/Categories";
+import FeaturedGuides from "./components/FeaturedGuides";
+import MidCTA from "./components/MidCTA";
+import Testimonials from "./components/Testimonials";
+import Security from "./components/Security";
+import FAQ from "./components/FAQ";
+import Freebies from "./components/Freebies";
+import JoinAsExpert from "./components/JoinAsExpert";
+import CartaNatalAvanzada from "./components/CartaNatalAvanzada";
 
 export default function Page() {
   return (
